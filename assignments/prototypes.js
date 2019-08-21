ef8d453a255b70de88bce38fdab07e3d4e93405e
Uzias Rivera <uziasrivera@gmail.com>
2019-08-21 16:06:46 -0700

    added some features in prototypes

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -40,7 +40,14 @@ function CharacterStats(attrs){
 }
 CharacterStats.prototype = Object.create(GameObject.prototype);
 CharacterStats.prototype.takeDamage = function(){//prototype
+  this.healthPoints = this.healthPoints - 2
   return `${this.name} took damage`
+}
+
+CharacterStats.prototype.die = function(){//prototype
+  totalDamage = this.healthPoints
+  this.healthPoints -= this.healthPoints
+  return `${this.name} took a total damage of ${totalDamage} and has died`
 }
 
 /*
@@ -64,7 +71,7 @@ function Humanoid(attrs){
 // }
 }
 Humanoid.prototype = Object.create(CharacterStats.prototype);
-//Humanoid.prototype = Object.create(GameObject.prototype);
+//for some reason, this needs to go on top. Didn't work when underneath
 Humanoid.prototype.greet = function () {
   return `${this.name} offers a greeting in ${this.language}`
 }
@@ -95,7 +102,7 @@ Humanoid.prototype.greet = function () {
     ],
     language: 'Common Tongue',
   });
-console.log(mage.greet())
+
 
   const swordsman = new Humanoid({
     createdAt: new Date(),
@@ -141,9 +148,58 @@ console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
+console.log(mage.healthPoints);
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+  function Villain(attrs){
+    Humanoid.call(this, attrs)
+    this.specialSupply = attrs.specialSupply
+  }
+
+  Villain.prototype = Object.create(Humanoid.prototype)
+  Villain.prototype.attack = function(human){
+    //this.specialSupply += 5
+   return (`${human.takeDamage()} from ${this.name}, ${human.name} now has ${human.healthPoints} health`)
+  }
+  Villain.prototype.specialAttack = function(human){
+    if (this.specialSupply>this.specialSupply/2){
+      this.specialSupply = this.specialSupply/2 -1
+    return (`${human.die()} at the hands of ${this.name}.....${human.destroy()}`)}
+    else{
+      console.log('You do not have enough Special Supply to use this attack!')
+    }
+
+   }
+
+  const thief = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Cujo',
+    team: 'Steet League',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Quechua',
+    specialSupply:10
+  });
+
+console.log(thief.attack(mage))
+console.log(thief.specialAttack(archer))
+console.log(thief.specialAttack(mage))
+console.log(thief.attack(mage))
+console.log(thief.attack(swordsman))
+
+
+// console.log(thief.attack(mage))
+
+
+  function Hero(){}
