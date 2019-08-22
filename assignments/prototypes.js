0d1757c0ec044ebec987b75e04c944170f53da1c
Uzias Rivera <uziasrivera@gmail.com>
2019-08-22 08:17:39 -0700

    finished villains attack, special attack, and added some more logic to the game

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -162,12 +162,16 @@ console.log(mage.healthPoints);
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+  
+  //Villain constructor
+  
   function Villain(attrs){
     Humanoid.call(this, attrs)
     //this.maxHP = attrs.maxHP
     this.specialSupply = attrs.specialSupply
   }
 
+  //inheriting humanoid methods
   Villain.prototype = Object.create(Humanoid.prototype)
   
   Villain.prototype.attack = function(human){
